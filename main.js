f2f3306472e2e5ff4482a66c8f6ae867b3c97cf1
Sheng Wu <shengwu34@gmail.com>
2017-07-04 15:23:19 -0700

    notifications

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const {app, Notification} = require('electron')
 const path = require('path')
 const url = require('url')
 const menubar = require('menubar')
@@ -5,6 +6,14 @@ const menubar = require('menubar')
 var mb = menubar()
 
 mb.on('ready', function ready () {
+
+  const notification = new Notification({
+    title: 'hello world',
+    body: 'Some other text'
+  })
+  notification.show()
+
+
   console.log('app is ready')
   // your app code here
 })
