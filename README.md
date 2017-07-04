40b5ee555b01b7e14bddf01952092f927b52f47f
Sheng Wu <shengwu34@gmail.com>
2017-07-04 15:01:19 -0700

    menubar stubs

---
+++ README.md
@@ -0,0 +1,6 @@
+Reminder
+========
+
+20-minute reminders.
+
+Based on advice from my eye doctor: for 20/20 vision, take a break every 20 minutes looking at something 20 feet away for 20 seconds.
---
+++ main.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const url = require('url')
+const menubar = require('menubar')
+
+var mb = menubar()
+
+mb.on('ready', function ready () {
+  console.log('app is ready')
+  // your app code here
+})
