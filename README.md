2fb6c3c9a994b3ec684452e94600a73aacc6e360
Sheng Wu <shengwu34@gmail.com>
2017-07-04 16:05:40 -0700

    reminders at 20 minute interval

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Reminder
 20-minute reminders.
 
 Based on advice from my eye doctor: for 20/20 vision, take a break every 20 minutes looking at something 20 feet away for 20 seconds.
+
+
+Credits
+=======
+Stopwatch icon from Alexander Bickov, Noun Project
+
+
+License
+=======
+MIT License
--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-const {app, Notification} = require('electron')
+const electron = require('electron')
+const {Notification} = electron
 const path = require('path')
 const url = require('url')
 const menubar = require('menubar')
 
-var mb = menubar()
+const ONE_MINUTE_INTERVAL = 60 * 1000
+const REMINDER_INTERVAL = 20 * ONE_MINUTE_INTERVAL
 
-mb.on('ready', function ready () {
+const intervalToStr = (interval_ms) => {
+  return interval_ms / ONE_MINUTE_INTERVAL + ' minutes'
+}
 
+const showNotification = (title, body, silent = false) => {
   const notification = new Notification({
-    title: 'hello world',
-    body: 'Some other text'
+    title: title,
+    body: body,
+    silent: silent
   })
   notification.show()
+}
 
+const showReminder = () => {
+  showNotification('Take a break!',
+                   'It has been ' + intervalToStr(REMINDER_INTERVAL))
+}
 
-  console.log('app is ready')
-  // your app code here
+const mb = menubar({
+  icon: 'icon.png'
+})
+
+mb.on('ready', () => {
+  showNotification('Starting reminders',
+                   'Interval: ' + intervalToStr(REMINDER_INTERVAL),
+                   true)
+  setInterval(showReminder, REMINDER_INTERVAL)
 })
